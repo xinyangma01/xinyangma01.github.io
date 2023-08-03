cfa14bd3e10e887b7327637d461ddbe970f1d87e
xinyangma01 <130402682+xinyangma01@users.noreply.github.com>
2023-08-03 00:10:38 -0400

    add files

---
+++ src/app.js
@@ -0,0 +1,60 @@
+const { reviewData } = window;
+
+window.addEventListener("load", function () {
+  createCard(reviewData);
+});
+
+const createCard = (reviewData) => {
+  reviewData.forEach((elem) => {
+    const cards = document.querySelector(".cards");
+    const card = document.createElement("article");
+    const uname = document.createElement("p");
+    const date = document.createElement("date");
+    const rating = document.createElement("p");
+    const detail = document.createElement("p");
+    cards.appendChild(card);
+    card.className = "card";
+    card.appendChild(uname);
+    uname.className = "uname";
+    uname.innerHTML = elem.uname;
+    card.appendChild(date);
+    date.innerHTML = elem.date;
+    card.appendChild(rating);
+    rating.className = "rating";
+    rating.innerHTML = "";
+    for (let i = 0; i < elem.rating; i++) {
+      rating.innerHTML += "★";
+    }
+    for (let i = 0; i < 5 - elem.rating; i++) {
+      rating.innerHTML += "☆";
+    }
+    card.appendChild(detail);
+    detail.className = "detail";
+    detail.innerHTML = elem.detail;
+  });
+};
+
+const review = document.querySelector("#submitreview");
+review.addEventListener("click", function (e) {
+  e.preventDefault();
+  clear(reviewData);
+  submitReview(reviewData);
+  createCard(reviewData);
+});
+
+const submitReview = (reviewData) => {
+  let newReview = {};
+  newReview.uname = document.getElementById("reviewername").value;
+  newReview.date = document.getElementById("reviewdate").value;
+  newReview.rating = document.getElementById("rating").value;
+  newReview.detail = document.getElementById("reviewdetail").value;
+  reviewData.push(newReview);
+};
+
+const clear = (reviewData) => {
+  const cards = document.querySelector(".cards");
+  const card = document.querySelectorAll(".card");
+  for (let i = 0; i < reviewData.length; i++) {
+    cards.removeChild(card[i]);
+  }
+};

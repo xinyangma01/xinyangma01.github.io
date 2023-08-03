7d65bdbb5c921845f29d9bcae90835bf7166b49f
xinyangma01 <130402682+xinyangma01@users.noreply.github.com>
2023-08-03 00:13:41 -0400

    add files

---
+++ data.js
@@ -0,0 +1,37 @@
+window.reviewData = [
+  {
+    uname: "Sophia Rivers",
+    date: "2021-03-24",
+    rating: 5,
+    detail:
+      "The variety of puzzles and challenges kept us entertained, and we never knew what delightful surprise awaited us in the next level."
+  },
+  {
+    uname: "Jackson Mercer",
+    date: "2021-06-17",
+    rating: 5,
+    detail:
+      "It Takes Two is more than just a game; it's an emotional journey that tugs at the heartstrings."
+  },
+  {
+    uname: "Mia Falcone",
+    date: "2021-04-30",
+    rating: 4,
+    detail:
+      "The beautifully designed environments and charming characters made us feel deeply connected to the story."
+  },
+  {
+    uname: "Liam Whitman",
+    date: "2021-08-09",
+    rating: 5,
+    detail:
+      "The gameplay is fun and varied throughout the entire run, and it frequently manages to surprise you."
+  },
+  {
+    uname: "Elijah Steele",
+    date: "2021-09-01",
+    rating: 5,
+    detail:
+      "Despite its flaws, It Takes Two is a special and highly polished game."
+  }
+];

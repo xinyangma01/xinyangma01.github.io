4e09126e56a0205f1c8753ded7f882dfcf90db13
xinyangma01 <130402682+xinyangma01@users.noreply.github.com>
2023-08-07 15:45:52 -0400

    update

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ window.addEventListener("load", function () {
   createCard(reviewData);
 });
 
+// generate a card
 const createCard = (reviewData) => {
   reviewData.forEach((elem) => {
     const cards = document.querySelector(".cards");
     const card = document.createElement("article");
-    const uname = document.createElement("p");
-    const date = document.createElement("date");
-    const rating = document.createElement("p");
-    const detail = document.createElement("p");
+    const uname = document.createElement("p"); // name
+    const date = document.createElement("date"); // date
+    const rating = document.createElement("p"); // rating
+    const detail = document.createElement("p"); // comments
     cards.appendChild(card);
     card.className = "card";
     card.appendChild(uname);
@@ -22,6 +23,7 @@ const createCard = (reviewData) => {
     card.appendChild(rating);
     rating.className = "rating";
     rating.innerHTML = "";
+    // display rating visually with filled and empty stars
     for (let i = 0; i < elem.rating; i++) {
       rating.innerHTML += "★";
     }
@@ -34,8 +36,10 @@ const createCard = (reviewData) => {
   });
 };
 
+//submit button event listener
 const review = document.querySelector("#submitreview");
 review.addEventListener("click", function (e) {
+  //only if all fields are filled
   if (
     document.querySelector("#reviewername").value != "" &&
     document.querySelector("#reviewdate").value != "" &&
@@ -43,10 +47,11 @@ review.addEventListener("click", function (e) {
     document.querySelector("#reviewdetail").value != ""
   ) {
     e.preventDefault();
-    clearCard(reviewData);
-    submitReview(reviewData);
-    createCard(reviewData);
-    clearData();
+    clearCard(reviewData); // first clear all cards
+    submitReview(reviewData); // store data in the array
+    createCard(reviewData); // re-create all cards
+    clearData(); // clear user input fields
+    // scroll back to top
     window.scrollTo({
       top: 0,
       behavior: "smooth"
@@ -54,6 +59,7 @@ review.addEventListener("click", function (e) {
   }
 });
 
+//store user input review data in the reviewData array
 const submitReview = (reviewData) => {
   let newReview = {};
   newReview.uname = document.getElementById("reviewername").value;
@@ -63,6 +69,7 @@ const submitReview = (reviewData) => {
   reviewData.push(newReview);
 };
 
+// clear currently-displayed cards
 const clearCard = (reviewData) => {
   const cards = document.querySelector(".cards");
   const card = document.querySelectorAll(".card");
@@ -71,6 +78,7 @@ const clearCard = (reviewData) => {
   }
 };
 
+// clear user input values in review form
 const clearData = () => {
   document.querySelector("#reviewername").value = "";
   document.querySelector("#reviewdate").value = "";
